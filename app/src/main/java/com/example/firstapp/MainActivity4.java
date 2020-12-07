5a38cc14c731bbbd22217a87a8656bb8869acb87
Saad BENDAOUD <saad.benda@gmail.com>
2020-12-08 00:28:18 +0100

    version 3.0

--- app/src/main/java/com/example/firstapp/MainActivity4.java
+++ app/src/main/java/com/example/firstapp/MainActivity4.java
@@ -2,6 +2,7 @@ package com.example.firstapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,8 +13,19 @@ public class MainActivity4 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
         Intent intent = getIntent();
-        int index;
+        int index = 0;
         if(intent.hasExtra("index_image")) index = intent.getIntExtra("index_image", -1);
+        if (index > -1){
+            int pic;
+            switch (index){
+                case 2: pic = R.drawable.xbox;break;
+                case 1: pic = R.drawable.playstation;break;
+                case 0: pic = R.drawable.desktop;break;
+                default: pic = -1;break;
+            }
+            ImageView img = (ImageView) findViewById(R.id.img0);
+            img.setImageResource(pic);
+        }
 
     }
 }
